52a30510b94c2c679e5e362920b875a7f94480a2
aLeuleu <97832618+aLeuleu@users.noreply.github.com>
2023-10-09 11:24:37 +0200

    utilitary function "readFileToString"

---
+++ include/utils.hpp
@@ -0,0 +1,13 @@
+//
+// Created by Aurelien Levra on 04/10/2023.
+//
+
+
+#ifndef UTILS_HPP
+#define UTILS_HPP
+
+#include <string>
+std::string readFileToString(const std::string& filename);
+
+
+#endif //UTILS_HPP

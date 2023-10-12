18cfcfb0f0a655205a6b717789fe6a86b624685f
aLeuleu <97832618+aLeuleu@users.noreply.github.com>
2023-10-12 12:08:54 +0200

    add "toString" function

--- include/utils/utils.hpp
+++ include/utils/utils.hpp
@@ -2,7 +2,26 @@
 #define UTILS_HPP
 
 #include <string>
-std::string readFileToString(const std::string& filename);
+enum colors {
+	RED,
+	GREEN,
+	YELLOW,
+	BLUE,
+	GREY,
+	PURPLE
+};
+std::string readFileToString(const std::string &filename);
+void printBold(const std::string &str);
+void printBoldThenThin(const std::string &str1, const std::string &str2);
+void coloredLog(const std::string &str1, const std::string &str2, colors color);
+#include <sstream>
 
+template<typename T>
+std::string toString(const T &value) {
+	std::ostringstream oss;
+	oss << value;
+	return oss.str();
+}
 
+std::string getDate();
 #endif //UTILS_HPP

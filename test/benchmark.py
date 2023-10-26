b2e9b0fd687f8655ab747bfefeadb1e3988bd368
tdameros <tomdamerose@gmail.com>
2023-10-26 16:58:18 +0200

    debug workflow

--- test/benchmark.py
+++ test/benchmark.py
@@ -1,9 +1,12 @@
 import unittest
 import subprocess
 import time
-import signal
+import inspect
 
 CONFIG_FILE = "test/conf/comments.conf"
+
+BENCHMARK_TIME_SECONDS = 30
+MINIMUM_AVAILABILITY_RATE = 95
 
 
 class TestBenchmark(unittest.TestCase):
@@ -11,22 +14,15 @@ class TestBenchmark(unittest.TestCase):
     def setUpClass(cls):
         print("start serv", flush=True)
         cls.server_process = subprocess.Popen(
-            ["./webserv", CONFIG_FILE], stdout=subprocess.DEVNULL,
+            ["./webserv", CONFIG_FILE],
+            stdout=subprocess.DEVNULL,
             stderr=subprocess.DEVNULL)
-        # print("communicate serv", flush=True)
-        # cls.stdout, cls.stderr = cls.server_process.communicate()
         time.sleep(2)
 
     @classmethod
     def tearDownClass(cls):
-        print("terminate serv", flush=True)
         cls.server_process.terminate()
-        print("terminate serv", flush=True)
-        cls.server_process.send_signal(signal.SIGINT)
-        print("terminate serv", flush=True)
         cls.server_process.wait()
-        print("server is down", flush=True)
-        # print(f"Server out: {cls.stdout}")
 
     @staticmethod
     def parse_availability(siege_content):
@@ -39,24 +35,20 @@ class TestBenchmark(unittest.TestCase):
         return float(availability)
 
     def test_single_page(self):
-        print("start test", flush=True)
         siege_process = subprocess.Popen(
-            ["siege", "-b", "-t30s", "http://127.0.0.1:9000/"],
+            ["siege", "-b", f"-t{30}s", "http://127.0.0.1:9000/"],
             stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         stdout, stderr = siege_process.communicate()
-        print("Wait siege", flush=True)
         siege_process.wait()
-        print("Finish wait siege", flush=True)
         self.assertEqual(siege_process.returncode, 0)
-        print(f"OUT : {stdout}")
-        print(f"ERR : {stderr}")
         availability = self.parse_availability(stdout.decode())
         if availability is None:
             availability = self.parse_availability(stderr.decode())
-        print(f"Availability: {availability}", flush=True)
+        current_method_name = inspect.currentframe().f_code.co_name
+        print(f"{current_method_name} availability: {availability}%", flush=True)
         if availability is None:
             self.fail("Fail parse availability")
-        self.assertGreaterEqual(availability, 95)
+        self.assertGreaterEqual(availability, MINIMUM_AVAILABILITY_RATE)
 
 
 if __name__ == "__main__":

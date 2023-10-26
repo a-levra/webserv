f89370a05bd691b43ca5330ba18ecc5fa74f707f
tdameros <tomdamerose@gmail.com>
2023-10-26 14:52:07 +0200

    debug workflow

--- test/benchmark.py
+++ test/benchmark.py
@@ -8,17 +8,17 @@ CONFIG_FILE = "test/conf/comments.conf"
 class TestBenchmark(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        print("start serv")
+        print("start serv", flush=True)
         cls.server_process = subprocess.Popen(
             ["./webserv", CONFIG_FILE],
             stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        print("communicate serv")
+        print("communicate serv", flush=True)
         cls.stdout, cls.stderr = cls.server_process.communicate()
         time.sleep(2)
 
     @classmethod
     def tearDownClass(cls):
-        print("terminate serv")
+        print("terminate serv", flush=True)
         cls.server_process.terminate()
         cls.server_process.wait()
         # print(f"Server out: {cls.stdout}")
@@ -34,19 +34,19 @@ class TestBenchmark(unittest.TestCase):
         return float(availability)
 
     def test_single_page(self):
-        print("start test")
+        print("start test", flush=True)
         siege_process = subprocess.Popen(
             ["siege", "-b", "-t30s", "http://localhost:9000/"],
             stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         stdout, stderr = siege_process.communicate()
-        print("Wait siege")
+        print("Wait siege", flush=True)
         siege_process.wait()
-        print("Finish wait siege")
+        print("Finish wait siege", flush=True)
         self.assertEqual(siege_process.returncode, 0)
         # print(stdout)
         # print(stderr)
         availability = self.parse_availability(stdout.decode())
-        print(f"Availability: {availability}")
+        print(f"Availability: {availability}", flush=True)
         if availability is None:
             self.fail("Fail parse availability")
         self.assertGreaterEqual(availability, 95)
